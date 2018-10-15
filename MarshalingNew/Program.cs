eeb0c21d9604efb6753e830ceac5768b04afc56c
Vitalii.Rogozhyn <vitalii.rogozhyn@gmail.com>
2018-10-15 21:46:04 +0300

    Not works

--- MarshalingNew/Program.cs
+++ MarshalingNew/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Runtime.InteropServices;
 namespace MarshalingNew
 {
     class Program
     {
+        private const string DllName = @"..\..\..\Debug\nfuncs.dll";
+       
+        [DllImport(DllName)]
+        public static extern int Add(int a, int b);
         static void Main(string[] args)
         {
+            int a = 30;
+            int b = 10;
+            int Sum = Program.Add(a, b);
+            Console.WriteLine(Sum);
         }
     }
 }
