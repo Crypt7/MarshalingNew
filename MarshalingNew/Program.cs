0c936d3bba7ce9015fc27134f98512bcb4e19dfb
Vitalii.Rogozhyn <vitalii.rogozhyn@gmail.com>
2018-10-16 19:00:33 +0300

    fixed

--- MarshalingNew/Program.cs
+++ MarshalingNew/Program.cs
@@ -11,12 +11,12 @@ namespace MarshalingNew
         private const string DllName = @"..\..\..\Debug\nfuncs.dll";
        
         [DllImport(DllName)]
-        public static extern int Add(int a, int b);
+        public static extern int add(int a, int b);
         static void Main(string[] args)
         {
             int a = 30;
             int b = 10;
-            int Sum = Program.Add(a, b);
+            int Sum = Program.add(a, b);
             Console.WriteLine(Sum);
         }
     }
